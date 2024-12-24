95535a60a711fdbc8726b3398fe6fb4830fafb29
Pushkardeep <just.pushkardeep@gmail.com>
2024-12-24 22:12:27 +0530

    Aniations

--- src/components/sliderMenu.jsx
+++ src/components/sliderMenu.jsx
@@ -7,7 +7,7 @@ function SliderMenu({ setMenu }) {
   const unMount = () => {
     gsap.to(".slider", {
       y: "-100%",
-      duration: 0.4,
+      duration: 0.2,
       onComplete: () => setMenu(false),
     });
   };
@@ -15,7 +15,7 @@ function SliderMenu({ setMenu }) {
   useGSAP(() => {
     gsap.from(".slider", {
       y: "-100%",
-      duration: 0.4,
+      duration: 0.2,
     });
   }, []);
 
@@ -24,36 +24,38 @@ function SliderMenu({ setMenu }) {
       data-scroll
       data-scroll-sticky
       data-scroll-target="#container"
-      className="slider sm:hidden w-full min-h-fit bg-white rounded-b-md fixed top-0 left-0 flex flex-col justify-center items-center gap-0.5 text-[#404040] font-['gil_med'] text-2xl z-[999999] py-4 px-4"
+      className="slider sm:hidden w-full min-h-screen bg-black/45 fixed top-0 left-0 z-[999999]"
     >
-      <div
-        onClick={unMount}
-        className="cursor-pointer w-full flex justify-end items-center gap-1"
-        aria-label="Close Menu"
-      >
-        <span className="text-[18px]">close</span>
-        <RiCloseLargeFill className="w-6 h-6 text-black aspect-square rounded-full" />
+      <div className="w-full min-h-fit bg-white rounded-b-md  flex flex-col justify-center items-center gap-0.5 text-[#404040] font-['gil_med'] text-2xl py-4 px-4">
+        <div
+          onClick={unMount}
+          className="cursor-pointer w-full flex justify-end items-center gap-1"
+          aria-label="Close Menu"
+        >
+          <span className="text-[18px]">close</span>
+          <RiCloseLargeFill className="w-6 h-6 text-black aspect-square rounded-full" />
+        </div>
+        <a className="mt-5" href="#" aria-label="Instagram">
+          Instagram
+        </a>
+        <hr className="w-[50%] border-[#b6b6b6] mx-auto" />
+        <a
+          href="https://www.linkedin.com/in/pushkardeep-khattri-0b0024312"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+        >
+          LinkedIn
+        </a>
+        <hr className="w-[50%] border-[#b6b6b6] mx-auto" />
+        <a href="#" aria-label="Github">
+          Github
+        </a>
+        <hr className="w-[50%] border-[#b6b6b6] mx-auto" />
+        <a href="mailto:just.puskardeep@gmail.com" aria-label="Email">
+          Email
+        </a>
       </div>
-      <a className="mt-5" href="#" aria-label="Instagram">
-        Instagram
-      </a>
-      <hr className="w-[50%] border-[#b6b6b6] mx-auto" />
-      <a
-        href="https://www.linkedin.com/in/pushkardeep-khattri-0b0024312"
-        target="_blank"
-        rel="noopener noreferrer"
-        aria-label="LinkedIn"
-      >
-        LinkedIn
-      </a>
-      <hr className="w-[50%] border-[#b6b6b6] mx-auto" />
-      <a href="#" aria-label="Github">
-        Github
-      </a>
-      <hr className="w-[50%] border-[#b6b6b6] mx-auto" />
-      <a href="mailto:just.puskardeep@gmail.com" aria-label="Email">
-        Email
-      </a>
     </div>
   );
 }
