18e6f3209a5c936850c3a2b2a50d7867a42349d7
Pushkardeep <just.pushkardeep@gmail.com>
2024-12-24 12:19:13 +0530

    vercel.json added

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // server: {
-  //   port: 3000,
-  //   host: "192.168.2.228",
-  // },
+  plugins: [react()]
 });
